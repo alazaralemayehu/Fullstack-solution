20f36d39696c42606cf85be7923b601e7f7deab4
booker <booker@booker.lan>
2020-12-18 00:36:45 +0200

    Part1 CourseInfo done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Part = (props) => {
+  return (
+    <p> {props.courseDetail.name} {props.courseDetail.exercise} </p>
+  )
+}
+const Course = (props) => {
+  return(
+      <h1> {props.coursename} </h1>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part courseDetail = {props.courseDetail[0]} />
+      <Part courseDetail = {props.courseDetail[1]} />
+      <Part courseDetail = {props.courseDetail[2]} />
+    </div>
+  )    
+}
+
+const Total = (props) => {
+  return(
+  <>
+    <p> Number of exercises {props.courseDetail[0].exercise +  props.courseDetail[1].exercise + props.courseDetail[2].exercise} </p>  
+  </>)
+}
+const App = () => {
+  const courseDetail = {
+    name : 'Half Stack application development',
+    details : [
+      {
+        name: 'Fundamentals of React',
+        exercise: 10,
+      } , 
+      {
+        name: 'Using props to pass data',
+        exercise: 7,
+      } , 
+      {
+        name: 'State of a component',
+        exercise: 14,
+      },
+    ]
+  }
+
+  return (
+    <>
+      <Course coursename= {courseDetail.name} />
+      <Content courseDetail= {courseDetail.details} />
+      <Total courseDetail = {courseDetail.details} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
